019cc04e1181d7f7614a84f27e2abcc955455ea5
rafaelnunesCodeGalaxy <55004937+rafaelnunesCodeGalaxy@users.noreply.github.com>
2020-02-14 11:23:53 -0300

    add text editor

--- src/Components/TextEditor.js
+++ src/Components/TextEditor.js
@@ -1,7 +1,24 @@
-import React, { Component } from 'react';
+/* eslint-disable default-case */
+import React, { useMemo, useState } from "react";
+import { Slate, Editable, withReact } from "slate-react";
+import { createEditor } from "slate";
 
-export default class Components extends Component {
-  render() {
-    return <div />;
+export default function TextEditor() {
+  const editor = useMemo(() => withReact(createEditor()), []);
+  const [value, setValue] = useState([
+    {
+      type: 'paragraph',
+      children: [{ text: 'A line of text in a paragraph.' }],
+    },
+  ]);
+
+  function keyDown(e, change) {
+    
   }
+
+  return (
+    <Slate editor={editor} value={value} onChange={value => setValue(value)}>
+      <Editable onKeyDown={keyDown} />
+    </Slate>
+  );
 }
